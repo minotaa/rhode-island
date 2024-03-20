98ed296d138d5fb9adcca678320f88dc42c6cd7a
minotaa <jpichardo369@gmail.com>
2024-03-20 15:42:24 -0400

    Game work!

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,29 +1,34 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta):
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * SPEED
-	var dir
-	if velocity.x > 0:
-		$Body.play("char1_walk_right")
-	elif velocity.x < 0:
-		$Body.play("char1_walk_left")
-	if velocity.y > 0:
-		$Body.play("char1_walk_down")
-	elif velocity.y < 0:
-		$Body.play("char1_walk_up")
-	if velocity.length() == 0:
-		$Body.play("char1_idle")
+	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")  
+	velocity.normalized()
 	
-
+	if round(velocity.x) == -1:
+		$Body.play("char1_walk_left")
+	if round(velocity.x) == 1:
+		$Body.play("char1_walk_right")
+	if round(velocity.y) == -1:
+		$Body.play("char1_walk_up")
+	if round(velocity.y) == 1:
+		$Body.play("char1_walk_down")
+	if round(velocity.x) == 1 and round(velocity.y) == 1:
+		$Body.play("char1_walk_right")
+	if round(velocity.x) == 1 and round(velocity.y) == -1:
+		$Body.play("char1_walk_right")
+	if round(velocity.x) == -1 and round(velocity.y) == -1:
+		$Body.play("char1_walk_left")
+	if round(velocity.x) == -1 and round(velocity.y) == 1:
+		$Body.play("char1_walk_left")	
+	print(round(velocity.x))
+	print(round(velocity.y))
+	velocity *= SPEED
+	if velocity.x == 0 and velocity.y == 0:
+		$Body.play("char1_idle")	
+	
 	move_and_slide()
