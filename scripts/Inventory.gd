40582cf8994af288efdc1be66c499c373bd5e9bd
minotaa <jpichardo369@gmail.com>
2024-03-26 17:30:40 -0400

    try to add fishing

--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -7,14 +7,14 @@ var max_capacity = 30
 func size() -> int:
 	return list.size()
 
-func get_item(index: int) -> Item:
+func get_item(index: int) -> ItemType:
 	return list[0]
 	
 func is_full() -> bool:
 	return list.size() >= max_capacity
 
-func add_item(item: Item) -> void:
+func add_item(item: ItemType) -> void:
 	list.append(item)
 
-func remove_item(item: Item) -> void:
+func remove_item(item: ItemType) -> void:
 	list.erase(item)
---
+++ scripts/Items.gd
@@ -0,0 +1,69 @@
+extends Object
+class_name Items
+
+var fish_list = []
+
+func fish_roll() -> Fish:
+	var totalWeight = 0
+	for item in fish_list:
+		totalWeight += item.reel_weight
+	var randomValue = randi() % totalWeight
+	var currentWeight = 0
+	for item in fish_list:
+		currentWeight += item.reel_weight
+		if randomValue < currentWeight:
+			return item
+	return null
+
+func _init():
+	var parrot_fish = Fish.new()
+	parrot_fish.name = "Parrot Fish"
+	parrot_fish.id = 0
+	parrot_fish.description = "Colorful reef dwellers with beak-like mouths that munch on algae, crucial for coral reef health and beach sand production."
+	parrot_fish.cost = 20.0
+	parrot_fish.sell_price = 5.5
+	parrot_fish.reel_weight = 50
+	parrot_fish.reel_location = "beach"
+	parrot_fish.atlas_region_w = 16.0
+	parrot_fish.atlas_region_h = 16.0
+	fish_list.append(parrot_fish)
+	
+	var mackerel = Fish.new()
+	mackerel.name = "Mackerel"
+	mackerel.id = 1
+	mackerel.description = "Swift, streamlined fish prized for their silvery scales and rich flavor. They're vital predators in marine ecosystems, feeding on smaller fish and plankton, and are popular catches for anglers and commercial fisheries alike."
+	mackerel.cost = 23.5
+	mackerel.sell_price = 7.5
+	mackerel.reel_weight = 45
+	mackerel.reel_location = "beach"
+	mackerel.atlas_region_x = 16.0
+	mackerel.atlas_region_w = 16.0
+	mackerel.atlas_region_h = 16.0
+	fish_list.append(mackerel)
+
+	var clown_fish = Fish.new()
+	clown_fish.name = "Clownfish"
+	clown_fish.id = 2
+	clown_fish.description = "Vibrant reef fish, famous for their symbiotic bond with sea anemones. Their striking colors and unique relationship make them iconic inhabitants of coral reefs."
+	clown_fish.cost = 21.0
+	clown_fish.sell_price = 6.5
+	clown_fish.reel_weight = 55
+	clown_fish.reel_location = "beach"
+	clown_fish.atlas_region_x = 32.0
+	clown_fish.atlas_region_w = 16.0
+	clown_fish.atlas_region_h = 16.0
+	fish_list.append(clown_fish)
+
+	var plaice = Fish.new()
+	plaice.name = "Plaice"
+	plaice.id = 3
+	plaice.description = "Flatfish with reddish-orange spots, commonly found in the North Atlantic and North Sea. prized for its delicate flavor and camouflaging ability against sandy or muddy seabeds."
+	plaice.cost = 23.5
+	plaice.sell_price = 8.5
+	plaice.reel_weight = 60
+	plaice.reel_location = "beach"
+	plaice.atlas_region_x = 48.0
+	plaice.atlas_region_w = 16.0
+	plaice.atlas_region_h = 16.0
+	fish_list.append(plaice)
+
