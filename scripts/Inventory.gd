0935d1815cac13ab4a44c836797f3451962f8fcb
starrybomp <starry@bomp.dev>
2024-04-08 01:06:10 -0400

    add inventory

--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -2,7 +2,7 @@ extends Object
 class_name Inventory
 
 var list = []
-var max_capacity = 30
+var max_capacity = 10
 
 func size() -> int:
 	return list.size()
@@ -14,9 +14,9 @@ func is_full() -> bool:
 	return list.size() >= max_capacity
 
 func add_item(item: ItemStack) -> void:
-	for items in list: # Checks for existing items.
-		if items.id == item.id:
-			var prev = list[list.find(items)]
+	for i in list: # Checks for existing items.
+		if i.type.id == item.type.id:
+			var prev = list[list.find(i)]
 			prev.amount += item.amount
 			return
 	list.append(item)
