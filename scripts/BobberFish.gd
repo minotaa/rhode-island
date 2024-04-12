cd733ec511a970dbdb7310387672edd2a1a1aa3f
starrybomp <starry@bomp.dev>
2024-04-12 10:12:31 -0400

    Add some kinda thing to reel back fish

---
+++ scripts/BobberFish.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var movement_speed = 4
+var movement_time = 1
+
+var min_distance = 100
+var max_distance = 200
+
+var min_position = 20
+var max_position = 290
+
+func _ready():
+	plan_move()
+	
+func plan_move():
+	var target = randf_range(min_position, max_position)
+	
+	while (abs(self.position.y - target) < min_distance or abs(self.position.y - target) > max_distance):
+		target = randf_range(min_position, max_position)
+		
+	move(Vector2(self.position.x, target))
+
+	
+func move(target):
+	var tween = create_tween()
+	
+	#tween.interpolate_value(self, "position", position, target, Tween.TRANS_QUINT, Tween.EASE_OUT)
+	tween.tween_property(self, "position", target, movement_speed).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.play()
+	
+	$MoveTimer.set_wait_time(movement_time)
+	$MoveTimer.start()
+
+func destroy():
+	get_parent().remove_child(self)
+	queue_free()
+func timeout():
+	plan_move()
