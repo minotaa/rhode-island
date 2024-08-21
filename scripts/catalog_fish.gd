c021227e75690aa844968fca68f1c3c145da81a2
minotaa <jpichardo369@gmail.com>
2024-08-21 18:39:27 -0400

    la mama

--- scripts/catalog_fish.gd
+++ scripts/catalog_fish.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var fish = preload("res://assets/tiles/fish_all.png")
-var type: int
+var type: Fish
 
 func set_amount(amount: int) -> void:
 	$Panel/Amount.text = "x" + str(amount)
@@ -14,3 +14,6 @@ func set_sprite(x, y, w, h):
 	atlas.atlas = fish
 	atlas.region = Rect2(x, y, w, h)
 	$Panel/TextureRect.texture = atlas
+
+func get_button() -> Button:
+	return $Panel
