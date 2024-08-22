4795441cb7a9a4ab2200e340a0e73c2c5e52e57a
minotaa <jpichardo369@gmail.com>
2024-08-22 12:58:51 -0400

    make game multiplayer

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,157 +1,23 @@
-extends Node
+extends Node2D
 
-var balance = 0.0
-var whiffs = 0
-var catches = 0 
-var highest_balance = balance
-var pos_x: float
-var pos_y: float
-var equipped_bait: Bait = Inventories.bait_bag.equipped
+@export var player_scene: PackedScene
 
-func calculate_blessing() -> int:
-	var blessing = 0
-	for item in Inventories.upgrade_bag.list:
-		if item.type.id == 2:
-			blessing += 25
-		if item.type.id == 3:
-			blessing += 25
-	blessing += Inventories.fishing_rods.equipped.blessing
-	if Inventories.bait_bag.equipped != null:
-		blessing += Inventories.bait_bag.equipped.bonus_blessing
-	return blessing
-
-func calculate_drop_multiplier(blessing: int) -> int:
-	var guaranteed_multiplier: int = blessing / 100
-	var next_drop_chance = (blessing % 100) / 100.0
-	var random_value = randf()
-	if random_value < next_drop_chance:
-		return guaranteed_multiplier + 1
+func _ready() -> void:
+	if not multiplayer.has_multiplayer_peer():
+		var p = load("res://scenes/player.tscn").instantiate()
+		add_child(p)
 	else:
-		return guaranteed_multiplier
+		Multiplayer.player_joined.connect(player_joined)
+		Multiplayer.player_quit.connect(player_quit)
 
-func get_bait_id() -> Variant:
-	if Inventories.bait_bag.equipped != null:
-		return Inventories.bait_bag.equipped.id
-	else:
-		return null
-
-func load_game():
-	if not FileAccess.file_exists("user://game.rtlbe"):
+func player_joined(id) -> void:
+	if not multiplayer.is_server():
 		return
-	var save_file = FileAccess.open("user://game.rtlbe", FileAccess.READ)
-	while save_file.get_position() < save_file.get_length():
-		var json_string = save_file.get_line()
-		var json = JSON.new()
-		
-		var parse_result = json.parse(json_string)
-		if not parse_result == OK:
-			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
-			continue
-		
-		var data = json.get_data()
-		if data.has("bag"):
-			Inventories.fishing_bag.set_list_from_save(data["bag"])
-		balance = data["coins"]
-		if data.has("whiffs"):
-			whiffs = data["whiffs"]
-		if data.has("catches"):
-			catches = data["catches"]
-		if data.has("pos_x"):
-			pos_x = data["pos_x"]
-		if data.has("pos_y"):
-			pos_y = data["pos_y"]
-		if data.has("fishing_rod"):
-			#print(data["fishing_rod"])
-			Inventories.fishing_rods.equipped = Items.get_rod_from_id(data["fishing_rod"])
-		if data.has("rods"):
-			Inventories.fishing_rods.set_list_from_save(data["rods"])
-		if data.has("catalog"):
-			Inventories.fishing_bag.collected = data["catalog"]
-		if data.has("baits"):
-			Inventories.bait_bag.set_list_from_save(data["baits"])
-		if data.has("upgrade_list"):
-			Inventories.upgrade_bag.set_list_from_save(data["upgrade_list"])
-		if data.has("clothing_bag"):
-			Inventories.clothing_bag.set_list_from_save(data["clothing_bag"])
-		if data.has("highest_balance"):
-			highest_balance = data["highest_balance"]
-		if data.has("items_bought"):
-			Inventories.items_bought = data["items_bought"]
-		if data.has("items_sold"):
-			Inventories.items_sold = data["items_sold"]
-		if data.has("equipped_clothes"):
-			if data["equipped_clothes"] != null:
-				Inventories.clothing_bag.equipped_clothes = data["equipped_clothes"]
-		if data.has("equipped_skin_tone"):
-			if data["equipped_skin_tone"] != null:
-				Inventories.clothing_bag.equipped_skin_tone = data["equipped_skin_tone"]
-		if data.has("equipped_shoes"):
-			if data["equipped_shoes"] != null:
-				Inventories.clothing_bag.equipped_shoes = data["equipped_shoes"]
-		if data.has("equipped_pants"):
-			if data["equipped_pants"] != null:
-				Inventories.clothing_bag.equipped_pants = data["equipped_pants"]
-		if data.has("equipped_acc"):
-			if data["equipped_acc"] != null:
-				Inventories.clothing_bag.equipped_acc = data["equipped_acc"]
-		if data.has("selected_bait"):
-			if data["selected_bait"] != null:
-				Inventories.bait_bag.equipped = Items.get_bait_from_id(data["selected_bait"])
-	print("Loaded the game.")
-	#print(Inventories.clothing_bag.list)
-
-func get_game_data() -> Dictionary:
-	if balance > highest_balance:
-		highest_balance = balance
-	if GameCenter.game_center != null:
-		GameCenter.game_center.post_score({
-			"score": highest_balance * 100.0,
-			"category": "highest_balance"
-		})
-		GameCenter.game_center.post_score({
-			"score": catches,
-			"category": "fish_caught"
-		})
-		GameCenter.game_center.post_score({
-			"score": whiffs,
-			"category": "fish_lost"
-		})
-		GameCenter.game_center.post_score({
-			"score": Inventories.items_bought,
-			"category": "items_bought"
-		})
-		GameCenter.game_center.post_score({
-			"score": Inventories.items_sold,
-			"category": "items_sold"
-		})
-		print("Updated Game Center scores.")
-	return {
-		"bag": Inventories.fishing_bag.to_list(),
-		"coins": balance,
-		"pos_x": pos_x,
-		"pos_y": pos_y,
-		"whiffs": whiffs,
-		"catches": catches,
-		"fishing_rod": Inventories.fishing_rods.equipped.id,
-		"rods": Inventories.fishing_rods.to_list(),
-		"catalog": Inventories.fishing_bag.collected,
-		"baits": Inventories.bait_bag.to_list(),
-		"selected_bait": get_bait_id(),
-		"upgrade_list": Inventories.upgrade_bag.to_list(),
-		"highest_balance": highest_balance,
-		"items_bought": Inventories.items_bought,
-		"items_sold": Inventories.items_sold,
-		"clothing_bag": Inventories.clothing_bag.to_list(),
-		"equipped_clothes": Inventories.clothing_bag.equipped_clothes,
-		"equipped_skin_tone": Inventories.clothing_bag.equipped_skin_tone,
-		"equipped_shoes": Inventories.clothing_bag.equipped_shoes,
-		"equipped_pants": Inventories.clothing_bag.equipped_pants,
-		"equipped_acc": Inventories.clothing_bag.equipped_acc
-	}
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child", player, true)
+	player.stop_fucking_up.rpc()
+	pass
 	
-func save_game(_data: Dictionary, reason: String):
-	var save_file = FileAccess.open("user://game.rtlbe", FileAccess.WRITE)
-	save_file.store_line(JSON.stringify(get_game_data()))
-	#for key in _data.keys():
-	#	save_file.store_line(JSON.stringify({key: _data[key]}))
-	print("Saved the game. " + "(" + reason + ")")
+func player_quit(id) -> void:
+	pass
---
+++ scripts/Multiplayer.gd
@@ -0,0 +1,99 @@
+extends Node
+
+var PORT: int = 1213
+const DEFAULT_SERVER_IP: String = "127.0.0.1"
+var players = []
+var main: PackedScene = preload("res://scenes/game.tscn")
+
+signal player_joined(peer_id)
+signal player_quit(peer_id)
+
+func _ready():
+	if DisplayServer.get_name() == "headless":
+		print("Detected client is using a headless version of the game, loading the game...")
+		get_tree().change_scene_to_packed(main)
+		print("Starting a server...")
+		var peer = ENetMultiplayerPeer.new()
+		var error = peer.create_server(PORT)
+		if error == OK:
+			print("Created server with IP " + DEFAULT_SERVER_IP + " with port " + str(PORT))
+		else:
+			print(error)
+			return
+		multiplayer.multiplayer_peer = peer
+		multiplayer.peer_connected.connect(_player_joined)
+		multiplayer.peer_disconnected.connect(_player_quit)
+
+# Server only	
+func _player_joined(id) -> void:
+	print("[server] player joined with the id " + str(id))
+	print("[server] sending to players")
+	server_player_joined.rpc(id)
+	pass
+
+func _player_quit(id) -> void:
+	print("[server] player quit with the id " + str(id))
+	print("[server] sending to players")
+	server_player_quit.rpc(id)
+	pass
+	
+# Client only
+
+@rpc("authority", "call_local", "reliable")
+func server_player_joined(id) -> void:
+	print("[" + str(multiplayer.multiplayer_peer.get_unique_id()) + "] [client] player joined with the id " + str(id))
+	player_joined.emit(id)
+	pass
+
+@rpc("authority", "call_local", "reliable")
+func server_player_quit(id) -> void:
+	print("[" + str(multiplayer.multiplayer_peer.get_unique_id()) + "] [client] player quit with the id " + str(id))
+	player_quit.emit(id)
+	pass
+
+func server_disconnected() -> void:
+	print("server disconnected")
+	multiplayer.server_disconnected.disconnect(server_disconnected)
+	multiplayer.connection_failed.disconnect(connection_failed)
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	multiplayer.multiplayer_peer = null
+
+func connection_failed() -> void:
+	print("connection failed")
+	multiplayer.server_disconnected.disconnect(server_disconnected)
+	multiplayer.connection_failed.disconnect(connection_failed)
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	multiplayer.multiplayer_peer = null
+	
+	
+func join_server(address: String, username: String = "Player") -> bool:
+	if not username.is_valid_identifier():
+		username = "Player"
+	if address == "localhost":
+		address = "127.0.0.1"
+	if not address.is_valid_ip_address():
+		print("Invalid IP Address")
+		return false
+	var split_address = address.split(":")
+	var valid_address: String
+	var port: int
+	if split_address.size() == 1:
+		valid_address = split_address[0]
+		port = PORT
+	elif split_address.size() > 2:
+		print("Too big arguments or something...")
+		return false
+	else:
+		valid_address = split_address[0]
+		port = split_address[1]
+	var peer = ENetMultiplayerPeer.new()
+	var error = peer.create_client(valid_address, port)
+	if error:
+		print(error)
+		print("Error occurred while connecting")
+		return false
+	multiplayer.multiplayer_peer = peer
+	multiplayer.server_disconnected.connect(server_disconnected)
+	multiplayer.connection_failed.connect(connection_failed)
+	print("[" + str(multiplayer.multiplayer_peer.get_unique_id()) + "] Connected to the server")
+	return true
