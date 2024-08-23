57f62d928ed3e048208d01f580d67e32409b2ffe
minotaa <jpichardo369@gmail.com>
2024-08-23 15:33:55 -0400

    Add a default server port & allow user to provide a server port

--- scripts/Multiplayer.gd
+++ scripts/Multiplayer.gd
@@ -11,6 +11,10 @@ signal update_players(players)
 signal player_quit(peer_id)
 
 func _ready():
+	var arguments = OS.get_cmdline_args()
+	for arg in arguments:
+		if arg.split("=")[0] == "--port":
+			PORT = arg.split("=")[1].to_int()
 	if DisplayServer.get_name() == "headless":
 		print("Detected client is using a headless version of the game, loading the game...")
 		get_tree().change_scene_to_packed(main)
